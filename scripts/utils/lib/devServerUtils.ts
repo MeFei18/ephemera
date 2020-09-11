5409c81bc4bad81c3975dd92319dc788584b30dd
Daisy <jfh52020@163.com>
2020-09-11 20:59:53 +0800

    choose_port

--- scripts/utils/lib/devServerUtils.ts
+++ scripts/utils/lib/devServerUtils.ts
@@ -1,6 +1,24 @@
 import net from "net";
 
+const listen = (port: number) => {
+    const server = net.createServer().listen(port);
+
+    return new Promise((resolve, reject) => {
+        server.on("listening", () => {
+            server.close();
+            resolve(port);
+        });
+
+        server.on("error", (err) => {
+            // reject("111");
+            resolve(listen(port + 1));
+        });
+    });
+};
+
 /**
  * 选择端口, 如果默认端口繁忙则选择新端口
  */
-export const choosePort = (host: string, defaultPort: number) => {};
+export const choosePort = (host: string, defaultPort: number) => {
+    listen(9000).then((p) => console.log(p));
+};
